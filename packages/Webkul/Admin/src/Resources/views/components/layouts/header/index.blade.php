40aeeb74364f28b021270da05dfcb35ce680d9fe
Osmir Pedro <osmirpedropedrodonascimentop@gmail.com>
2025-10-01 22:08:34 -0300

    remove old brand part2

--- packages/Webkul/Admin/src/Resources/views/components/layouts/header/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/layouts/header/index.blade.php
@@ -78,18 +78,6 @@
 
             <!-- Admin Dropdown -->
             <x-slot:content class="mt-2 border-t-0 !p-0">
-                <div class="flex items-center gap-1.5 border border-x-0 border-b-gray-300 px-5 py-2.5 dark:border-gray-800">
-                    <img
-                        src="{{ url('cache/logo.png') }}"
-                        width="24"
-                        height="24"
-                    />
-
-                    <!-- Version -->
-                    <p class="text-gray-400">
-                        @lang('admin::app.layouts.app-version', ['version' => core()->version()])
-                    </p>
-                </div>
 
                 <div class="grid gap-1 pb-2.5">
                     <a
