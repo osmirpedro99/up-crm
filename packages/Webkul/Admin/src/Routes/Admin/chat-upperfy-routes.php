f8459d78b11a064e9ffff98faae801eb37566a54
Osmir Pedro <osmir.pedro@invenis.com.br>
2025-10-24 11:34:42 -0300

    update url chat

--- packages/Webkul/Admin/src/Routes/Admin/chat-upperfy-routes.php
+++ packages/Webkul/Admin/src/Routes/Admin/chat-upperfy-routes.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('external', function () {
-    return redirect()->away('https://chat.upperfy.com.br');
+    return redirect()->away('https://upperchat.upperfy.com.br');
 })->name('admin.chat.upperfy');
